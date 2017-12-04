c84c755c07071f6dfeaf6090b44189097e37ace4
rkpkr <30536578+rkpkr@users.noreply.github.com>
2017-12-03 21:57:30 -0600

    Added years/days left in your life module.

---
+++ modules/timeleft.py
@@ -0,0 +1,62 @@
+import datetime
+
+def time_left(gender, year, month, day):
+    if gender == 'M' or gender == 'MALE' or gender == 'BOY' or gender == 'MAN':
+        cur_year = datetime.date.today().year
+        cur_month = datetime.date.today().month
+        cur_day = datetime.date.today().day
+        if cur_month >= month and cur_day >= day:
+            age = cur_year - year
+            years_left = 82 - age
+            next_bday = datetime.date((cur_year + 1), month, day)
+            to_bday = next_bday - datetime.date.today()
+            days1 = to_bday.days
+            days_left = days1 + ((years_left - 1) * 365)
+            print('You have ' + str(years_left) +
+             ' years left to live, or approximately ' + str(days_left) + ' days.')
+            return None
+        elif cur_month < month or (cur_month >= month and cur_day < day):
+            age = cur_year - year - 1
+            years_left = 82 - age
+            next_bday = datetime.date(cur_year, month, day)
+            to_bday = next_bday - datetime.date.today()
+            days1 = to_bday.days
+            days_left = days1 + ((years_left - 1) * 365)
+            print('You have ' + str(years_left) +
+             ' years left to live, or approximately ' + str(days_left) + ' days.')
+            return None
+    elif gender == 'F' or gender == 'FEMALE' or gender == 'GIRL' or gender == 'WOMAN':
+        cur_year = datetime.date.today().year
+        cur_month = datetime.date.today().month
+        cur_day = datetime.date.today().day
+        if cur_month >= month and cur_day >= day:
+            age = cur_year - year
+            years_left = 87 - age
+            next_bday = datetime.date((cur_year + 1), month, day)
+            to_bday = next_bday - datetime.date.today()
+            days1 = to_bday.days
+            days_left = days1 + ((years_left - 1) * 365)
+            print('You have ' + str(years_left) +
+             ' years left to live, or approximately ' + str(days_left) + ' days.')
+            return None
+        elif cur_month < month or (cur_month >= month and cur_day < day):
+            age = cur_year - year - 1
+            years_left = 87 - age
+            next_bday = datetime.date(cur_year, month, day)
+            to_bday = next_bday - datetime.date.today()
+            days1 = to_bday.days
+            days_left = days1 + ((years_left - 1) * 365)
+            print('You have ' + str(years_left) +
+             ' years left to live, or approximately ' + str(days_left) + ' days.')
+            return None
+    else:
+        print('Invalid parameter(s).')
+        return False
+
+
+if __name__ == '__main__':
+    time_left('M', 1999, 12, 25)
+    time_left('FEMALE', 1936, 2, 1)
+    time_left('gal', 2001, 9, 11)
+    time_left('BOY', 2016, 12, 5)
+    time_left('MAN', 1986, 10, 27)
--- tellme.py
+++ tellme.py
@@ -3,6 +3,7 @@ from modules.crypto import get_price, currency_check
 from modules.weather import galv_weather, caribou_weather
 from modules.xmas import days_till_xmas
 from modules.hnews import hacker_news
+from modules.timeleft import time_left
 
 def btc(args):
     if not currency_check(args.fiat.upper()):
@@ -25,8 +26,12 @@ def xmas(args):
     days_till_xmas()
 
 def hnews(args):
-    print('\n')
+    print('\n', end='')
     hacker_news(int(args.stories))
+
+def timeleft(args):
+    print('\n', end='')
+    time_left(args.gender.upper(), int(args.year), int(args.month), int(args.day))
 
 parser = argparse.ArgumentParser()
 subparsers = parser.add_subparsers()
@@ -45,6 +50,14 @@ xmas_parser.set_defaults(func=xmas)
 hnews_parser = subparsers.add_parser('hnews')
 hnews_parser.add_argument('stories', nargs='?', default=5, const=5)
 hnews_parser.set_defaults(func=hnews)
+
+timeleft_parser = subparsers.add_parser('timeleft')
+timeleft_parser.add_argument('gender')
+timeleft_parser.add_argument('year')
+timeleft_parser.add_argument('month')
+timeleft_parser.add_argument('day')
+timeleft_parser.set_defaults(func=timeleft)
+
 
 if __name__ == '__main__':
     args = parser.parse_args()
