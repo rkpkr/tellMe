36f3c1ccbaf89ba672b910ed57cbbd6cefb1460e
rkpkr <30536578+rkpkr@users.noreply.github.com>
2017-12-04 18:57:50 -0600

    Refactored and fixed timeleft module.

--- modules/timeleft.py
+++ modules/timeleft.py
@@ -2,56 +2,45 @@ import datetime
 
 def time_left(gender, year, month, day):
     if gender == 'M' or gender == 'MALE' or gender == 'BOY' or gender == 'MAN':
-        cur_year = datetime.date.today().year
-        cur_month = datetime.date.today().month
-        cur_day = datetime.date.today().day
-        if cur_month >= month and cur_day >= day:
-            age = cur_year - year
-            years_left = 82 - age
-            next_bday = datetime.date((cur_year + 1), month, day)
-            to_bday = next_bday - datetime.date.today()
-            days1 = to_bday.days
-            days_left = days1 + ((years_left - 1) * 365)
-            print('You have ' + str(years_left) +
-             ' years left to live, or approximately ' + str(days_left) + ' days.')
-            return None
-        elif cur_month < month or (cur_month >= month and cur_day < day):
-            age = cur_year - year - 1
-            years_left = 82 - age
-            next_bday = datetime.date(cur_year, month, day)
-            to_bday = next_bday - datetime.date.today()
-            days1 = to_bday.days
-            days_left = days1 + ((years_left - 1) * 365)
-            print('You have ' + str(years_left) +
-             ' years left to live, or approximately ' + str(days_left) + ' days.')
-            return None
+        est_left = 82
     elif gender == 'F' or gender == 'FEMALE' or gender == 'GIRL' or gender == 'WOMAN':
-        cur_year = datetime.date.today().year
-        cur_month = datetime.date.today().month
-        cur_day = datetime.date.today().day
-        if cur_month >= month and cur_day >= day:
-            age = cur_year - year
-            years_left = 87 - age
-            next_bday = datetime.date((cur_year + 1), month, day)
-            to_bday = next_bday - datetime.date.today()
-            days1 = to_bday.days
-            days_left = days1 + ((years_left - 1) * 365)
-            print('You have ' + str(years_left) +
-             ' years left to live, or approximately ' + str(days_left) + ' days.')
-            return None
-        elif cur_month < month or (cur_month >= month and cur_day < day):
-            age = cur_year - year - 1
-            years_left = 87 - age
-            next_bday = datetime.date(cur_year, month, day)
-            to_bday = next_bday - datetime.date.today()
-            days1 = to_bday.days
-            days_left = days1 + ((years_left - 1) * 365)
-            print('You have ' + str(years_left) +
-             ' years left to live, or approximately ' + str(days_left) + ' days.')
-            return None
+        est_left = 87
     else:
         print('Invalid parameter(s).')
         return False
+    cur_year = datetime.date.today().year
+    cur_month = datetime.date.today().month
+    cur_day = datetime.date.today().day
+    if cur_month > month:
+        age = cur_year - year
+        years_left = est_left - age
+        next_bday = datetime.date((cur_year + 1), month, day)
+        to_bday = next_bday - datetime.date.today()
+        days1 = to_bday.days
+        days_left = days1 + ((years_left - 1) * 365)
+        print('You have ' + str(years_left) +
+         ' years left to live, or approximately ' + str(days_left) + ' days.')
+        return None
+    elif cur_month == month and cur_day >= day:
+        age = cur_year - year
+        years_left = est_left - age
+        next_bday = datetime.date((cur_year + 1), month, day)
+        to_bday = next_bday - datetime.date.today()
+        days1 = to_bday.days
+        days_left = days1 + ((years_left - 1) * 365)
+        print('You have ' + str(years_left) +
+         ' years left to live, or approximately ' + str(days_left) + ' days.')
+        return None
+    else:
+        age = cur_year - year - 1
+        years_left = est_left - age
+        next_bday = datetime.date(cur_year, month, day)
+        to_bday = next_bday - datetime.date.today()
+        days1 = to_bday.days
+        days_left = days1 + ((years_left - 1) * 365)
+        print('You have ' + str(years_left) +
+         ' years left to live, or approximately ' + str(days_left) + ' days.')
+        return None
 
 
 if __name__ == '__main__':
